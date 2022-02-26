6ab995ebe02281a21113c9e01570d343c74e4318
Andrei <l2.ls@yandex.ru>
2022-02-26 15:11:56 +0300

    Реализация ДЗ. Генерация случайных объектов размещения

--- js/main.js
+++ js/main.js
@@ -1,3 +1,83 @@
+const offersData = {
+  TITLES: [
+    'Скромная квартирка',
+    'Роскошный пентхаус',
+    'Хлипкое бунгало',
+    'Очаровательный подвал',
+    'Семейное гнёздышко',
+    'Просторный чулан'
+  ],
+  DESCRIPTOINS: [
+    'Вам захочется провести здесь остаток жизни',
+    'Больше, чем просто жильё',
+    'Дружелюбные тараканы скрасят Ваше одиночество',
+    'Прекрасный вариант со всеми удобствами во дворе',
+    'Лучшее предложение в городе',
+    'Жилье от перекупов, дёшево и надёжно (нет)'
+  ],
+  AVATARS: [
+    'img/avatars/user01.png',
+    'img/avatars/user02.png',
+    'img/avatars/user03.png',
+    'img/avatars/user04.png',
+    'img/avatars/user05.png',
+    'img/avatars/user06.png',
+    'img/avatars/user07.png',
+    'img/avatars/user08.png',
+    'img/avatars/user09.png',
+    'img/avatars/user10.png',
+  ],
+  PHOTOS: ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+  ],
+  TYPES: ['palace', 'flat', 'house', 'bungalow', 'hotel'],
+  CHECKIN_TIME: ['12:00', '13:00', '14:00'],
+  CHECKOUT_TIME: ['12:00', '13:00', '14:00'],
+  FEATURES: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']
+};
+
+const {
+  TITLES,
+  DESCRIPTOINS,
+  AVATARS,
+  PHOTOS,
+  TYPES,
+  CHECKIN_TIME,
+  CHECKOUT_TIME,
+  FEATURES
+} = offersData;
+
+const offerTerms = {
+  ROOMS_MIN: 1,
+  ROOMS_MAX: 100,
+  PRICE_MIN: 10,
+  PRICE_MAX: 100000,
+  GUESTS_MIN: 1,
+  GUESTS_MAX: 10,
+  LAT_MIN: 35.65000,
+  LAT_MAX: 35.70000,
+  LNG_MIN: 139.70000,
+  LNG_MAX: 139.80000,
+  COORD_DIGITS: 5
+};
+
+const {
+  ROOMS_MIN,
+  ROOMS_MAX,
+  PRICE_MIN,
+  PRICE_MAX,
+  GUESTS_MIN,
+  GUESTS_MAX,
+  LAT_MIN,
+  LAT_MAX,
+  LNG_MIN,
+  LNG_MAX,
+  COORD_DIGITS
+} = offerTerms;
+
+const OFFERS_TOTAL = 10;
+
 const getRandomPositiveInteger = (a, b) => {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
@@ -15,3 +95,46 @@ const getRandomPositiveFloat = (a, b, digits = 1) => {
 
   return +result.toFixed(digits);
 };
+
+const getRandomArrayElement = (array) => array[getRandomPositiveInteger(0, array.length - 1)];
+
+const getChangedArray = (array) => {
+  const copy = array.slice();
+  const newArray = [];
+
+  for (let i = getRandomPositiveInteger(1, copy.length); i > 0; i--) {
+    newArray.push(copy.splice(getRandomPositiveInteger(0, copy.length - 1), 1)[0]);
+  }
+
+  return newArray;
+};
+
+const createOffer = function () {
+  const offerLat = getRandomPositiveFloat(LAT_MIN, LAT_MAX, COORD_DIGITS);
+  const offerLng = getRandomPositiveFloat(LNG_MIN, LNG_MAX, COORD_DIGITS);
+
+  return {
+    author: {
+      avatar: AVATARS.splice(getRandomPositiveInteger(0, AVATARS.length - 1), 1)[0]
+    },
+    offer: {
+      title: getRandomArrayElement(TITLES),
+      address: `${offerLat}, ${offerLng}`,
+      price: getRandomPositiveInteger(PRICE_MIN, PRICE_MAX),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomPositiveInteger(ROOMS_MIN, ROOMS_MAX),
+      guests: getRandomPositiveInteger(GUESTS_MIN, GUESTS_MAX),
+      checkin: getRandomArrayElement(CHECKIN_TIME),
+      checkout: getRandomArrayElement(CHECKOUT_TIME),
+      features: getChangedArray(FEATURES),
+      description: getRandomArrayElement(DESCRIPTOINS),
+      photos: getChangedArray(PHOTOS)
+    },
+    location: {
+      lat: offerLat,
+      lng: offerLng
+    }
+  };
+};
+
+Array.from({length: OFFERS_TOTAL}, createOffer);
